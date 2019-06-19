9de3a8123601673864de78f7ea6a6c71dc1b2ae0
Yashipoo <35319298+Yashipoo@users.noreply.github.com>
2019-06-18 23:45:26 -0500

    Update MainPlayer.java

--- core/src/com/mygdx/game/MainPlayer.java
+++ core/src/com/mygdx/game/MainPlayer.java
@@ -21,7 +21,7 @@ public class MainPlayer extends Game
 
 
     public static final short GROUND_BIT = 1;
-    public static final short MARIO_BIT = 2;
+    public static final short PRANAY_BIT = 2;
     public static final short BRICK_BIT = 4;
     public static final short COIN_BIT = 8;
     public static final short DESTROYED_BIT = 16;
