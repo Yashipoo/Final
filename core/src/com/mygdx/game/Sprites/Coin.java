209eb6c5c3164414fa5cb5114d56315777739c3b
Yashipoo <35319298+Yashipoo@users.noreply.github.com>
2019-06-18 23:23:59 -0500

    Update Coin.java

--- core/src/com/mygdx/game/Sprites/Coin.java
+++ core/src/com/mygdx/game/Sprites/Coin.java
@@ -18,21 +18,32 @@ public class Coin extends TileObject
 {
     private static TiledMapTileSet set;
 
-    private final int BLANK_COIN_INDEX = 28;
+    private final int BLANK_COIN_INDEX = 28; 
+    //index of the tile in the tileset
 
     public Coin(PlayScreen screen, Rectangle bounds)
     {
         super(screen, bounds);
         set = map.getTileSets().getTileSet("tileset");
-        fixture.setUserData(this); //so that we have access to this specific object, same for brick
+        //where you got the tiles from 
+        
+        fixture.setUserData(this); 
+        //so that we have access to this specific object, same for brick
+        
         setCategoryFilter(MainPlayer.COIN_BIT);
+        //What are we working with here. In this case, it is a coin
     }
 
     @Override
     public void onHeadHit()
     {
         Gdx.app.log("Coin", "Collision");
+        //So that we know that a collision has occurred
+        
         getCell().setTile(set.getTile(BLANK_COIN_INDEX));
+        //this will tell us which coin block did we hit
+        
         Hud.addScore(300);
+        //since we know that a collision has occurred, add points to your score
     }
 }
