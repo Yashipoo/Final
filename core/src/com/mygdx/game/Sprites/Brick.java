25fbe23f029a184bfdaa706c0dd368408cdbfd92
Yashipoo <35319298+Yashipoo@users.noreply.github.com>
2019-06-18 23:28:16 -0500

    Update Brick.java

--- core/src/com/mygdx/game/Sprites/Brick.java
+++ core/src/com/mygdx/game/Sprites/Brick.java
@@ -15,14 +15,22 @@ public class Brick extends TileObject
         super(screen, bounds);
         fixture.setUserData(this);
         setCategoryFilter(MainPlayer.BRICK_BIT);
+        //what object we are working with
     }
 
     @Override
     public void onHeadHit()
     {
         Gdx.app.log("Brick", "Collision");
+        //so that a message is sent saying that a collision with a brick has occurred
+        
         setCategoryFilter(MainPlayer.DESTROYED_BIT);
+        //Since a collision has occurred we want the destroyed brick
+        
         getCell().setTile(null);
+        //by setting this to null we are saying that we want nothing in that cell, so the cell is empty and the brick is destroyed
+        
         Hud.addScore(200);
+        //you gett 200 points for breaking a brick
     }
 }
